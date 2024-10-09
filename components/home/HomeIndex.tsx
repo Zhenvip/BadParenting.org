3123f142f4bc5b65631827f2b99aecc82a9692fe
Zhenvip <liangzhenvip@gmail.com>
2024-10-10 01:01:26 +0800

    Update HomeIndex.tsx

--- components/home/HomeIndex.tsx
+++ components/home/HomeIndex.tsx
@@ -2,7 +2,7 @@ import CTA from "@/components/home/CTA";
 import FAQ from "@/components/home/FAQ";
 import Feature from "@/components/home/Feature";
 import Hero from "@/components/home/Hero";
-import Pricing from "@/components/home/Pricing";
+import Download from "@/components/home/Download";
 import ScrollingLogos from "@/components/home/ScrollingLogos";
 import Showcase from "@/components/home/Showcase";
 import SocialProof from "@/components/home/SocialProof";
@@ -27,8 +27,8 @@ export default async function HomeIndex({ lang }: { lang: string }) {
       {/* USP (Unique Selling Proposition) */}
       <Feature id="Features" locale={dict.Feature} langName={langName} />
 
-      {/* Pricing */}
-      <Pricing id="Pricing" locale={dict.Pricing} langName={langName} />
+      {/* Download */}
+      <Download id="Download" locale={dict.Download} langName={langName} />
 
       {/* Testimonials */}
       <Testimonials id="Testimonials" locale={dict.Testimonials} />
