876ee2b2d946e6316746230c966c2c6c761a7d66
Zhenvip <liangzhenvip@gmail.com>
2024-10-10 09:15:04 +0800

    Update CTAButton.tsx

--- components/home/CTAButton.tsx
+++ components/home/CTAButton.tsx
@@ -1,20 +1,21 @@
 import { Button } from "@/components/ui/button";
-import { RocketIcon } from "lucide-react";
+import { DownloadIcon } from "lucide-react"; // Changed from RocketIcon to DownloadIcon
 import Link from "next/link";
 
 const CTAButton = ({ locale }: { locale: any }) => {
   return (
     <Link
-      href="https://BadParenting.org/"
+      href="https://github.com/Zhenvip/BadParenting.org/raw/master/Bad%20Parenting.rar"
       target="_blank"
       rel="noopener noreferrer nofollow"
+      download="Bad_Parenting.rar" // Added download attribute
     >
       <Button
         variant="default"
         className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white"
-        aria-label="Play Bad Parenting Now  "
+        aria-label="Download Bad Parenting Now"
       >
-        <RocketIcon />
+        <DownloadIcon /> {/* Changed to DownloadIcon */}
         {locale.title}
       </Button>
     </Link>
