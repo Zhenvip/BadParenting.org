60ca3324cfe71506a4d54b4028576ecdb1f7a662
Zhenvip <liangzhenvip@gmail.com>
2024-10-10 10:03:27 +0800

    Update tiers.ts

--- config/tiers.ts
+++ config/tiers.ts
@@ -32,7 +32,7 @@ export const TIERS_EN: Array<Tier> = [
       "One-on-one service",
       "More exquisite pages",
     ],
-    buttonText: "Contact us",
+    buttonText: "Support Me",
     buttonColor: "default",
     buttonVariant: "flat",
   },
